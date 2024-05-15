d1e1237a651e0cc772b6be5b4ddca4848075944d
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-15 21:57:47 +0530

    fixed serializtion using properties.yml

--- src/main/java/org/kafka/producer/config/KafkaProducerConfig.java
+++ src/main/java/org/kafka/producer/config/KafkaProducerConfig.java
@@ -12,12 +12,12 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+//@Configuration
 public class KafkaProducerConfig {
 
     @Bean
-    public Map<String, Object> producerConfig(){
-        Map<String, Object> properties= new HashMap<>();
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> properties = new HashMap<>();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
@@ -25,12 +25,12 @@ public class KafkaProducerConfig {
     }
 
     @Bean
-    public ProducerFactory<String,Object> producerFactory(){
+    public ProducerFactory<String, Object> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(){
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
